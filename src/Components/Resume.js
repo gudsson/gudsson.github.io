d955dddf243cb58b7f8cf404e8d7e1d1aec25d59
Jay Gudsson <gudsson@gmail.com>
2021-12-19 16:39:32 -0800

    adds synapse to resume sectino; g

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -14,19 +14,66 @@ class Resume extends Component {
             <div className="row item">
               <div className="twelve columns">
                 <div>
+                  <h3>Synapse GraphQL Gateway</h3>
+                  <p className="info">
+                    Co-Creator & Software Developer<span>&bull;</span>
+                    <em className="date">2021</em>
+                  </p>
+                  <ul>
+                    <p>
+                      Synapse is an open-source tool to build, deploy, and
+                      monitor a GraphQL Gateway, putting legacy APIs behind a
+                      single GraphQL endpoint. Visit the{" "}
+                      <a href="https://synapse-gateway.github.io">
+                        project website
+                      </a>{" "}
+                      to learn more.
+                    </p>
+                    <li>
+                      Developed a GUI for easily adding various API types to the
+                      Gateway
+                    </li>
+                    <li>
+                      Engineered an Apollo Server Plugin to log error data for
+                      GraphQL requests
+                    </li>
+                    <li>
+                      Captured and stored real-time resolver latency data in
+                      MongoDB
+                    </li>
+                    <li>
+                      Implemented a dashboard for monitoring request and error
+                      data
+                    </li>
+                    <li>
+                      Built a pipeline to provision and deploy to serverless AWS
+                      infrastructure
+                    </li>
+                    <li>
+                      Created a Node.js CLI to reduce total calls into a few
+                      simple commands
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <br />
+            <div className="row item">
+              <div className="twelve columns">
+                <div>
                   <h3>BC Building Science & Sense Engineering</h3>
                   <p className="info">
-                    Civil Engineer-in-Training <span>&bull;</span>
+                    Civil Engineer-in-Training<span>&bull;</span>
                     <em className="date">2016 - 2020</em>
                   </p>
                   <ul>
                     <li>
                       Consulted on building envelope and structural remediation
-                      work.
+                      work
                     </li>
                     <li>
                       Automated structural and financial analysis spreadsheets
-                      using VBA.
+                      using VBA
                     </li>
                   </ul>
                 </div>
@@ -46,13 +93,13 @@ class Resume extends Component {
                 <div>
                   <h3>Launch School</h3>
                   <p className="info">
-                    Online Software Engineering Program <span>&bull;</span>
+                    Online Software Engineering Program<span>&bull;</span>
                     <em className="date">2020 - 2021</em>
                   </p>
                   <ul>
                     <li>Mastery-based learning of vanilla JavaScript</li>
                     <li>Database foundations with PostgreSQL</li>
-                    <li>Front-End Development with Handlebars and jQuery.</li>
+                    <li>Front-End Development with Handlebars and jQuery</li>
                   </ul>
                 </div>
               </div>
@@ -63,7 +110,7 @@ class Resume extends Component {
                 <div>
                   <h3>University of British Columbia </h3>
                   <p className="info">
-                    Bachelor of Applied Science (Civil Engineering){" "}
+                    Bachelor of Applied Science (Civil Engineering)
                     <span>&bull;</span>
                     <em className="date">2009 - 2014</em>
                   </p>
@@ -71,7 +118,7 @@ class Resume extends Component {
                     <li>Gradulated with Distinction (3.95 GPA)</li>
                     <li>
                       Advanced courses in BIM Modelling, Concrete Design, and
-                      Structural Analysis.
+                      Structural Analysis
                     </li>
                   </ul>
                 </div>
